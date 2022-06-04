5d1836962d814c542fd2dfdae57ddd8e00b7429a
erulan <erulan14@yandex.ru>
2022-06-04 19:06:11 +0600

    Add choices model to account

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, Cards
 
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = ("user", "phone", "birth_date", "location")
+
+
+@admin.register(Cards)
+class CardsAdmin(admin.ModelAdmin):
+    list_display = ("name", "card_number", "card_code", "card_release", "card_ending", "balance")
--- main/models.py
+++ main/models.py
@@ -3,10 +3,14 @@ from django.contrib.auth.models import User
 
 
 class Cards(models.Model):
+    """
+    Модель выпуска карты
+    """
+
     name = models.CharField(max_length=20, verbose_name="Имя карты")
     card_number = models.CharField(max_length=16, verbose_name="Номер карты")
     card_code = models.IntegerField(verbose_name="Код карты")
-    card_release = models.DateField(verbose_name="Выпуск карты")
+    card_release = models.DateField(verbose_name="Выпуск карты", auto_now=True)
     card_ending = models.DateField(verbose_name="Срок карты")
 
     balance = models.DecimalField(default=0, max_digits=13, decimal_places=2, verbose_name="Баланс")
@@ -20,16 +24,32 @@ class Cards(models.Model):
         verbose_name_plural = "Карты"
 
 
-
 class Account(models.Model):
+    """
+    Модель учетной записи
+    """
+
+    LOCATION_CHOICES = (
+        ("SH", "Шымкент"),
+        ("AL", "Алматы"),
+        ("NS", "Нұр-Султан"),
+        ("TZ", "Тараз"),
+        ("AK", "Актау"),
+        ("AT", "Атырау"),
+        ("OS", "Өскемен"),
+        ("KT", "Көкшетау"),
+        ("KR", "Қарағанды"),
+        ("KO", "Қызылорда"),
+        ("TR", "Түркістан"),
+        ("TK", "Талдықорған"),
+    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
     phone = models.CharField(max_length=15, verbose_name="Номер телефона")
     birth_date = models.DateField(verbose_name="Дата рождения")
     avatar = models.ImageField(upload_to="account/avatar/", verbose_name="Фото профиля")
-    location = models.CharField(max_length=40, verbose_name="Локация")
+    location = models.CharField(max_length=40, verbose_name="Локация", choices=LOCATION_CHOICES)
     cards = models.ManyToManyField(Cards, verbose_name="Карты")
-
-    #balance = models.DecimalField(default=0, max_digits=13, decimal_places=2, verbose_name="Баланс")
 
     def __str__(self):
         return "{}".format(self.phone)
